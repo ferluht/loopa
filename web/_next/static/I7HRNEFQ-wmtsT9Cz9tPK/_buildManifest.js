4a3fdd1d0c6fb88747c06bc55be5e0e79a174803
ferluht <ilya.belikov@phystech.edu>
2024-04-24 22:40:51 +0400

    landing page

---
+++ web/_next/static/I7HRNEFQ-wmtsT9Cz9tPK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/192-ff5c99706a0f9171.js","static/chunks/pages/index-756b8157235523e6.js"],"/_error":["static/chunks/pages/_error-3c9a37b9888d2d4e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
